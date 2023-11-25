b88a150926208fc38d0bb712485a45cecf3714f4
Omar Gutierrez <omargtdev@gmail.com>
2023-11-25 08:46:49 -0500

    Receipt service added

---
+++ src/services/receipt.service.js
@@ -0,0 +1,24 @@
+import env from "../config/env";
+import apiService from "./api.service";
+import RequestModel from "./models/RequestModel";
+
+const addReceipt = async (dataRequest) => {
+	const request = new RequestModel(`${env.TRANSACTIONAL_API_URL}/receipts`, new FormData());
+	request.deleteHeader("Content-Type");
+	request.body.append("referralGuide", dataRequest.file);
+	request.body.append("dataJsonContent", JSON.stringify(dataRequest.jsonContent));
+	console.log(request.body.get("dataJsonContent"));
+
+	const { isOk, data, errorMessage } = await apiService.POST(request, false);
+
+	return {
+		isOk,
+		receiptId: data,
+		errorMessage,
+	};
+};
+
+export default {
+	addReceipt
+}
+
